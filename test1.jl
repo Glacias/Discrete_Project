428b47f3640f6c008556cbcb9db06da349d00956
Glacias <klapkaivan@yahoo.fr>
2021-04-24 20:40:17 +0200

    First question done

--- test1.jl
+++ test1.jl
@@ -3,6 +3,7 @@ using Gurobi
 using CSV
 using DataFrames
 using Bijections
+using LinearAlgebra
 
 # Read the data
 DATA_dir = joinpath("hsa") # Path to the data
@@ -30,3 +31,34 @@ for i in 1:DataFrames.nrow(raw_df)
 		net_mat[b(raw_df[i, 2]), b(raw_df[i, 1])] = 1
 	end
 end
+
+# Write a model and an algorithm that finds the largest module 
+# where the module is defined as a set of genes in which 
+# all pairwise co-expressions are present
+
+model = Model(Gurobi.Optimizer)
+
+## Variables
+
+# X_i = 1 if gene i is in the module
+#     = 0 otherwise
+
+@variable(model, x[i=1:n_genes], Bin)
+
+## Objective
+
+# Max sum x_i
+
+@objective(model, Max, sum(x))
+
+## Constraints
+
+# X_i + X_j <= 1 + net_mat[i,j] (with i=/=j if diag is 0)
+
+@constraint(model, [i = 1:n_genes, j = 1:n_genes; i != j], x[i] + x[j] <= 1 + net_mat[i,j])
+
+## Solve
+optimize!(model)
+
+# Get solution
+x_sol = value.(x)
